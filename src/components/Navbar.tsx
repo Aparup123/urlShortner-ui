a675f7c8e2957d9a8a3d4d47632eca0707370648
Aparup123 <aparuproychowdhury@gmail.com>
2025-12-15 00:31:33 +0530

    add favicon and title and description

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Unlink } from "lucide-react"
 import { Button } from "./ui/button"
 
 export const Navbar = () => {
   return (
-    <div className="flex justify-between">
-        <div>LOGO</div>
+    <div className="flex justify-between items-center">
+        <div className="font-bold text-xl flex justify-between gap-2 items-center cursor-pointer ">
+          <div>S<span className="text-green-400">hr</span>nk</div>
+          <span><Unlink/></span></div>
         <div className="flex md:gap-4 gap-2">
             <Button variant="outline">Sign in</Button>
             <Button>Sign up</Button>
